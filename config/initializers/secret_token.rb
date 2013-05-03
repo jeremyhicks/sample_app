58ea27250c2572cb3557529017c3073eada45f7c
Jeremy <mail@slaphost.com>
2013-05-03 16:43:26 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '167f07d4c09d365580afb0630e504f0336eec657554499ebd976088be234acc1423275c7f3c509aa3bebd1b74f855da8fee7c6455269021a2a90a04186992c41'
